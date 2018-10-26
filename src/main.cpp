f5314edf20bcf6947bb41fb3c5926e4c6e539b92
pr0crustes <dark.pr0crustes@gmail.com>
2018-10-26 19:28:54 -0300

    moved files

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 #include "gamewindow.h"
+
 #include <QApplication>
+#include <QScreen>
+#include <QDesktopWidget>
+#include <QStyle>
 
 
 int main(int argc, char *argv[]) {
 	QApplication a(argc, argv);
 	GameWindow w;
+
+	w.adjustSize();
+	w.setGeometry(
+		QStyle::alignedRect(
+			Qt::LeftToRight,
+			Qt::AlignCenter,
+			w.size(),
+			QGuiApplication::screens().first()->availableGeometry()
+		)
+	);
+
 	w.show();
 	return a.exec();
 }
